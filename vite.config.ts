6ccd5abeba2d6ba76c4371ea1a99c1c2da8a2429
Ilmi <ilmirasyidah10@gmail.com>
2025-07-25 14:04:14 +0700

    chore: setup svg import as component

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+      include: '**/*.svg?react',
+    }),
+  ],
   resolve: {
     alias: {
-     '@': path.resolve(__dirname, './src/'),
-      }
-    }
+      '@': path.resolve(__dirname, './src/'),
+    },
+  },
 })
